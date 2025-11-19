3e7033be0623d256231ba326bd6d8cd56d40770a
vkhao <vkhao@Xubuntu.myguest.virtualbox.org>
2025-11-19 17:14:35 +0100

    v0.2.0
    Changelog:
    
    Worked on an UI version of the game.
    Imported buttons and game mechanics.
    Revisited code.

--- main.js
+++ main.js
@@ -1,56 +1,63 @@
 let humanScore = 0
 let computerScore = 0
+let roundToWin = 5
+
+const button = document.querySelectorAll('#btn')
+const score = document.querySelector('#score')
+const result = document.querySelector('.results')
+
+const updateDisplay = () => {
+    score.textContent = `Player: ${humanScore} | Computer: ${computerScore}`
+}
 
 const getComputerChoice = () => {
-    const array = ['Rock', 'Paper', 'Scissor']
+    const array = ['Rock', 'Paper', 'Scissors']
     const choice = Math.floor(Math.random()*3)
     const upperCase = array[choice].toUpperCase()
     return upperCase
     }
 
-const getPlayerChoice = () => {
-    while (true) {
-        let choice = prompt('Enter your choice!', '')
-        const upperCase = choice.toUpperCase()
-
-        if (choice===null || choice.trim()==='') {
-            console.log('Action cancelled, retry.')
-            continue
-        } else if (upperCase!=='ROCK' && upperCase!=='PAPER' && upperCase!=='SCISSOR') {
-            console.log('Invalid input. Try again.')
-            continue
-        } else return upperCase
-    }
-}
-
-const getSelection = () => {
-    const humanSelection = getPlayerChoice()
-    const computerSelection = getComputerChoice()
-    return {human:humanSelection, computer:computerSelection}
+const getPlayerChoice = (event) => {
+    return event.currentTarget.className.split(' ')[0].toUpperCase();
 }
 
 const playRound = (humanChoice, computerChoice) => {
-    console.log(`Player chose ${humanChoice}, Computer chose ${computerChoice}`)
-    const winConditions = {'ROCK':'SCISSOR','SCISSOR':'PAPER','PAPER':'ROCK'}
+    result.textContent = ''
+    const winConditions = {'ROCK':'SCISSORS','SCISSORS':'PAPER','PAPER':'ROCK'}
     if (humanChoice === computerChoice) {
-        console.log('This round was a tie!')
+        result.textContent ='This round was a tie!'
     } else if (winConditions[humanChoice] === computerChoice) {
-        console.log('The player gets the score!')
+        result.textContent='The player gets the score!'
         humanScore += 1
     } else {
-        console.log('The computer gets the score!')
+        result.textContent='The computer gets the score!'
         computerScore += 1
     }
+    updateDisplay()
 }
 
-const playGame = (n) => {
-    humanScore = 0
-    computerScore = 0
-    for (let i = 0; i < n; i++) {
-        const selections = getSelection()
-        playRound(selections.human, selections.computer)
+const handlePlayerChoice = (event) => {
+    if (humanScore >= roundToWin || computerScore >= roundToWin) {
+        score.textContent = ''
+        humanScore = 0;
+        computerScore = 0;
+        updateDisplay()
+        result.textContent = 'New Game Started!'
+        return; 
     }
-    humanScore>computerScore ? console.log(`Player wins with ${humanScore} points!`) : computerScore>humanScore ? console.log (`Computer wins with ${computerScore} points!`) : console.log("It's a tie!")
+
+    const humanSelection = getPlayerChoice(event)
+    const computerSelection = getComputerChoice()
+
+    playRound(humanSelection, computerSelection)
+
+    if (humanScore >= roundToWin) {
+        result.textContent = `The Player won the game with ${humanScore} points! Click any button to start a new game.`;
+    } else if (computerScore >= roundToWin) {
+        result.textContent = `The Computer won the game with ${computerScore} points! Click any button to start a new game.`;
+    }
 }
 
-playGame(5)
+button.forEach(pressed => {
+    pressed.addEventListener('click', handlePlayerChoice)
+});
